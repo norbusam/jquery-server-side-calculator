c688cf01af8a815e14da3878a2d60194ccea616d
Your Name <norbusamphel@Norbus-MacBook-Pro.local>
2020-10-09 16:32:53 -0500

    clear function to clear the inputs created

--- server/public/script/scripts.js
+++ server/public/script/scripts.js
@@ -5,8 +5,22 @@ $(document).ready(onReady);
 function onReady() {
     console.log('JQ loaded');
     $('#submitInputs').on('click', submitInputs)
+    $('#clearInputs').on('click', clear)
 }// end of onReady
+
+function clear() {
+    $('#firstInput').val('');
+    $('#secondInput').val('');
+}//end of clear
 
 function submitInputs(){
     console.log('submitted!!');
-}
+    let operator = $('#plus').val();
+    console.log(operator);
+    let numbers = {
+        inputOne: $('#firstInput').val(),
+        operator: '',
+        inputTwo: $('#secondInput').val(),
+    }
+    console.log(numbers);
+}// end of submitInputs
