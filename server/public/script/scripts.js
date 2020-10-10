b0ea2dd27cdf3d8ed5293c424d2a02f6f262b219
Your Name <norbusamphel@Norbus-MacBook-Pro.local>
2020-10-10 11:43:03 -0500

    added comments on client and server side

--- server/public/script/scripts.js
+++ server/public/script/scripts.js
@@ -11,20 +11,25 @@ function onReady() {
 
 let operator = '';
 
+// a function to clear the input fields on DOM
 function clearInput() {
     $('#firstInput').val('');
     $('#secondInput').val('');
 }//end of clearInput
 
+// a GET route to grab the data from the server and to append to DOM
 function getHistory() {
     
 }// end of GetHistory
 
+// grabs which operator is clicked on DOM to be send to server
 function getOperator(){
     operator = $(this).text();
     console.log('grabbing the operator',operator);
-}
+}//end of getOperator
 
+//on submit button, a POST route to server and get the calculation done 
+//and runs getHistory(that's the plan for now)
 function submitInputs(){
     console.log('submitted!!');
     console.log(operator);
--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
+// Require to pass in express and body-parser
 const express = require('express');
 const bodyParser = require('body-parser');
-
+// app is declared to use express
 const app = express();
-
+// the port where the project is at
 const port = 5000;
 
 app.use(express.static('server/public'));
